5a369a2d20cd18bb30b7debb1555c5523b514c4e
mikkamp <michael.kamp@automattic.com>
2025-01-22 13:32:23 +0000

    Clear up HPOS warning

--- plugins/grow-smooth-generator/grow-smooth-generator.php
+++ plugins/grow-smooth-generator/grow-smooth-generator.php
@@ -61,3 +61,13 @@ function wc_grow_smooth_generator_notices() {
 	</div>
 	<?php
 }
+
+// WooCommerce compatibility declarations.
+add_action(
+	'before_woocommerce_init',
+	function () {
+		if ( class_exists( Automattic\WooCommerce\Utilities\FeaturesUtil::class ) ) {
+			Automattic\WooCommerce\Utilities\FeaturesUtil::declare_compatibility( 'custom_order_tables', __FILE__ );
+		}
+	}
+);
